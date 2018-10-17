298b91e97f2adf21ee5785a5962c10acd5bba154
Jakub Nowacki <j.s.nowacki@gmail.com>
2018-10-17 09:49:07 +0200

    WIP: SLS setup

---
+++ handler.py
@@ -0,0 +1,19 @@
+import json
+import logging
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def wiki(event, context):
+    logging.warn('Event: {}'.format(event))
