d5e19255958512d1a6ba875abcb0d98cbb24929c
Jakub Nowacki <j.s.nowacki@gmail.com>
2019-12-12 21:49:29 +0100

    Cloud run files initially added

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from wiki import parse_main
+
+app = Flask(__name__)
+
+
+@app.route("/crawl", methods=['POST'])
+def crawl():
+    lang = request.json['lang']
+
+    res = parse_main(lang)
+    if res is None:
+        res = {"message": f"Error! Unsupported language {lang}"}
+
+    return jsonify, res
+
+
+@app.route("/")
+def healthcheck():
+    return "OK"
